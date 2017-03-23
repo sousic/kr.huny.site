6992753098822c61edd4edc3e46f7484bb03ee6d
sousic <sousics@naver.com>
2017-03-23 18:45:54 +0900

    로그인 > spring secruity 적용 중

--- src/main/java/kr/huny/site/authentication/site/UserInfo.java
+++ src/main/java/kr/huny/site/authentication/site/UserInfo.java
@@ -75,22 +75,22 @@ public class UserInfo implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 
     public void setAuthorities(Set<GrantedAuthority> authorities) {
